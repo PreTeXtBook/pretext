944929e7bba8ede2eefa34b1771582eb5a308285
Harald Schilly <harald.schilly@gmail.com>
2016-05-21 21:54:43 +0000

    Jade example: improvements, verbatim source included (PR #276)

--- examples/jade/build.sh
+++ examples/jade/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# for jade, install node.js and then run npm install
-./node_modules/.bin/jade --extension xml jade.jade
+# for jade, install node.js and then run npm install (will read packages.json)
+./node_modules/.bin/jade --pretty --extension xml jade.jade
 xsltproc ../../xsl/mathbook-html.xsl jade.xml
 
