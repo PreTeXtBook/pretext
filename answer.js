476f315ea9ed28420c34534e81ec3c8325beb112
David W. Farmer <farmer@aimath.org>
2018-11-16 09:56:50 -0500

    initial commit

---
+++ answer.js
@@ -0,0 +1,211 @@
+
+
+Storage.prototype.setObject = function(key, value) {
+//    this.setItem(key, JSON.stringify(value));
+    this.setItem(key, JSON.stringify(value, function(key, val) {
+    return val.toFixed ? Number(val.toFixed(3)) : val;
+}));
+}
+
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+console.log('22222222222222 222222222222222 222222222222');
+
+var reading_questions = document.querySelectorAll("section.reading-questions article.exercise-like");
+
+console.log('aaaaa reading_questions.length', reading_questions.length);
+console.log('reading_questions 1', reading_questions[0]);
+console.log('reading_questions 2', reading_questions[1]);
+
+
+rq_answer_label = '<span'
+rq_answer_label += ' class="readingquestion_make_answer addcontent';
+rq_answer_label += ' ' + role + '"';
+rq_answer_label += ' style="margin-left:1em; font-size:80%; color:#a0a;"';
+rq_answer_label += '>';
+if (role == "instructor") {
+    rq_answer_label += 'Responses';
+} else {
+    rq_answer_label += 'Answer&rarr;';
+}
+rq_answer_label +='</span>';
+
+for (var j=0; j < reading_questions.length; ++j) {
+    var reading_question = reading_questions[j];
+    var reading_question_id = reading_question.id;
+
+    console.log("reading_question_id, last_child_type", reading_question_id, reading_question, "zzzz",reading_question.firstChild, "ggggg", reading_question.lastElementChild.tagName);
+    rq_answer_id = reading_question_id + "_text";
+    var existing_content = localStorage.getObject(rq_answer_id);
+    console.log(rq_answer_id, "existing_rq_content", existing_content);
+
+    if (reading_question.lastElementChild.tagName === "P") {
+        console.log("ends in a p");
+        reading_question.lastElementChild.innerHTML += rq_answer_label;
+    } else {
+       var this_answer_link = document.createElement('div');
+       this_answer_link.innerHTML = rq_answer_label;
+       reading_question.insertAdjacentElement("afterend", this_answer_link);
+    }
+    if (existing_content) {
+       console.log("RQ_existing_content", reading_question_id, reading_question);
+       console.log("those children", $('#'+reading_question_id).children(".readingquestion_make_answer"));
+       $('#'+reading_question_id).find(".readingquestion_make_answer").addClass("hidecontrols");
+
+       var this_rq_id_text = reading_question_id + "_text";
+       var this_rq_id_controls = reading_question_id + "_controls";
+       var answer_pre = '<pre';
+       answer_pre += ' id="' + this_rq_id_text + '"'
+       answer_pre += ' rows="' + '3' + '"';
+       answer_pre += ' style="width:100%; margin-top:0.5em;"';
+       answer_pre += '>';
+       answer_pre += existing_content
+       answer_pre += '</pre>';
+
+
+       var this_rq_controls = '<div id="' + this_rq_id_controls + '" class="input_controls hidecontrols" style="margin-bottom:-1.9em;">';
+       this_rq_controls += '<span class="action clear_item rq_delete">delete</span><span class="action save_item rq_edit">edit</span>';
+       this_rq_controls += '</div>'
+
+       var this_rq_answer_and_controls = document.createElement('div');
+       this_rq_answer_and_controls.setAttribute('style', 'width:80%; margin-left:auto; margin-right:auto; margin-top:0.5em;');
+       this_rq_answer_and_controls.setAttribute('class', 'rq_answer');
+       console.log("about to show the existing answer on #", reading_question_id);
+       this_rq_answer_and_controls.innerHTML = answer_pre + this_rq_controls;
+       $('#'+reading_question_id).append(this_rq_answer_and_controls);
+     //  this.parentNode.insertAdjacentElement("afterend", this_rq_answer_and_controls);
+
+    }
+
+}
+
+$('.readingquestion_make_answer').mousedown(function(e){
+  console.log(".readingquestion_make_answer");
+  $(this).addClass("hidecontrols");
+//  var this_canvas_id = this.parentNode.nextSibling.id;
+  var this_rq_id = this.parentNode.parentNode.id;
+  var this_rq_id_text = this_rq_id + "_text";
+  var this_rq_id_controls = this_rq_id + "_controls";
+  console.log(".rq", this_rq_id);
+  answer_textarea = '<textarea';
+  answer_textarea += ' id="' + this_rq_id_text + '"'
+  answer_textarea += ' rows="' + '3' + '"';
+  answer_textarea += ' style="width:100%;"';
+  answer_textarea += '>';
+  answer_textarea += '</textarea>';
+
+  var this_rq_controls = '<div id="' + this_rq_id_controls + '" class="input_controls" style="margin-bottom:-1.9em;">';
+  this_rq_controls += '<span class="action clear_item rq_delete">delete</span> <span class="action save_item rq_save">save</span>';
+  this_rq_controls += '</div>'
+
+  var this_rq_answer_and_controls = document.createElement('div');
+  this_rq_answer_and_controls.setAttribute('style', 'width:80%; margin-left:auto; margin-right:auto; margin-top:0.5em;');
+//  this_rq_answer_and_controls.setAttribute('z-index', '2000');
+  this_rq_answer_and_controls.innerHTML = answer_textarea + this_rq_controls;
+  this.parentNode.insertAdjacentElement("afterend", this_rq_answer_and_controls);
+
+  var textarea = window.document.querySelector("textarea");
+  textarea.addEventListener("keypress", function() {
+     if(textarea.scrollTop != 0){
+        textarea.style.height = textarea.scrollHeight + "px";
+     }
+  }, false);
+
+});
+
+// does not work because the content is dynamically loaded later
+// $('.rq_save').mousedown(function(e){
+$('body').on('click','.rq_save', function(){
+  console.log(".rq_save");
+//  var this_canvas_id = this.parentNode.nextSibling.id;
+  var this_rq_id = this.parentNode.previousSibling.id;
+  var this_rq_ans = this.parentNode.previousSibling;
+  console.log(".rq_save", this_rq_id);
+  console.log("this_rq_ans", this_rq_ans);
+  var this_rq_text = this_rq_ans.value;
+  this_rq_text = this_rq_text.trim();
+  console.log("this_rq_text", this_rq_text);
+  localStorage.setObject(this_rq_id, this_rq_text);
+
+  var this_ans_static = document.createElement('pre');
+  this_ans_static.setAttribute('id', this_rq_id);
+  this_ans_static.setAttribute('style', "margin-top: 0.5em;");
+  this_ans_static.innerHTML = this_rq_text
+  this_rq_ans.replaceWith(this_ans_static);
+
+  $(this).parent().addClass("hidecontrols");
+
+  $(this).parent().parent().addClass("rq_answer");
+
+  var edit_button = document.createElement('span');
+  edit_button.setAttribute('class', "action edit_item rq_edit");
+  edit_button.innerHTML = "edit";
+  this.replaceWith(edit_button);
+});
+
+$('body').on('click','.rq_edit', function(){
+  console.log(".rq_edit");
+//  var this_canvas_id = this.parentNode.nextSibling.id;
+  var this_rq_id = this.parentNode.previousSibling.id;
+  var this_rq_ans = this.parentNode.previousSibling;
+  console.log(".rq_edit", this_rq_id);
+  console.log("this_rq_ans", this_rq_ans);
+  var this_rq_text = this_rq_ans.innerHTML;
+  console.log("this_rq_text", this_rq_text);
+
+   //this is copied from above.  need to eliminate repeated code
+
+  var answer_textarea_editable = document.createElement('textarea');
+  answer_textarea_editable.setAttribute('id', this_rq_id);
+  answer_textarea_editable.setAttribute('rows', '3');
+  answer_textarea_editable.setAttribute('style', 'width:100%;');
+
+  this_rq_ans.replaceWith(answer_textarea_editable);
+//  this_rq_ans.innerHTML = answer_textarea;
+//  this_rq_ans.replaceWith(this_ans_static);
+
+  $(this).parent().removeClass("hidecontrols");
+
+  $(this).parent().parent().removeClass("rq_answer");
+
+  var save_button = document.createElement('span');
+  save_button.setAttribute('class', "action edit_item rq_save");
+  save_button.innerHTML = "save";
+  this.replaceWith(save_button);
+
+  $('#' + this_rq_id).val(this_rq_text);
+
+  var textarea = window.document.querySelector("textarea");
+  textarea.style.height = textarea.scrollHeight + "px";
+  textarea.addEventListener("keypress", function() {
+     if(textarea.scrollTop != 0){
+        textarea.style.height = textarea.scrollHeight + "px";
+     }
+  }, false);
+});
+
+$('body').on('mouseover','.rq_answer', function(){
+  console.log(".rq_answer");
+  $(this).children().last().removeClass("hidecontrols");
+  $(this).attr('z-index', '2000');
+});
+$('body').on('mouseleave','.rq_answer', function(){
+  console.log(".rq_answer");
+  $(this).children().last().addClass("hidecontrols");
+  $(this).attr('z-index', '');
+});
+
+$('body').on('click','.rq_delete', function(){
+  console.log(".rq_delete");
+  var this_rq_id = this.parentNode.previousSibling.id;
+  console.log(".rq_delete", this_rq_id);
+  $(this).parent().parent().prev().children(".readingquestion_make_answer").removeClass("hidecontrols");
+  $(this).parent().parent().remove();
+  localStorage.removeItem(this_rq_id);
+
+});
+
+
---
+++ login.js
@@ -0,0 +1,195 @@
+
+console.log("enabling login");
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+/*	document.cookie = name+"="+value+expires+"; path=/; domain=aimath.org";
+*/
+	document.cookie = name+"="+value+expires+"; path=/";
+        console.log("created cookie " + name);
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+function hash_of_string(str) {
+    str = str.toString();
+    var the_len = str.length;
+    the_len = Math.min(12,the_len);
+    var the_hash = 123456;
+    var hash_lis = [510149, 120151, 230157, 411063, 320167, 631973, 410179, 321081, 231091, 111093, 121097, 230199];
+    for (var j=0; j < the_len; ++ j) {
+        var this_char = str.charCodeAt(j);
+        this_char = parseInt(this_char);
+        var this_add = this_char * hash_lis[j];
+        the_hash += this_add;
+        the_hash = the_hash % 1000000;
+    }
+    return the_hash.toString()
+}
+
+function login_form(mode="login") {
+    var the_form = "";
+
+/*    the_form += '<div id="theloginform" class="modal login">';
+*/
+  if (mode == 'logout') {
+    the_form += '<form name="logoutform" class="modal-content animate" onSubmit="return removeLogin();" action="">';
+    the_form += '<div class="container">\n';
+    the_form += '<button type="submit">Really logout</button>';
+    the_form += '</div>\n';
+    the_form += '</form>\n';
+  }
+  else{
+    the_form += '<form name="loginform" class="modal-content animate" onSubmit="return validateLogin();" action="">';
+    if ((typeof guest_access !== 'undefined') && guest_access) {
+        the_form += '<p class="instructions" >You can log in as "guest" with password "guest" for 3 hours of access.</p>'; }
+    the_form += '<div class="container">\n';
+    the_form += '<label><b>Id</b></label>\n<input type="text" placeholder="Enter Username" name="uname" required>';
+    the_form += '<label><b>Password</b></label>\n<input type="password" placeholder="Enter Password" name="psw" required>';
+    the_form += '<button type="submit">Login</button>';
+    the_form += '</div>\n';
+    the_form += '<div class="container" style="background-color:#f1f1f1">';
+    the_form += '<span class="psw">Login trouble? Email <a href="mailto:help@aimath.org">help@aimath.org</a></span>';
+    the_form += '</div>\n';
+    the_form += '</form>\n';
+  }
+
+  theform = document.createElement('div');
+  theform.id = "the" + mode + "form";
+  theform.className = "modal login";
+  document.body.appendChild(theform);
+  theform.innerHTML = the_form;
+}
+
+function loadScript(script) {
+  var newscript = document.createElement('script');
+  newscript.type = 'text/javascript';
+  newscript.async = true;
+  newscript.src = 'https://aimath.org/~farmer/tmp/' + script + '.js';
+  var allscripts = document.getElementsByTagName('script');
+  var s = allscripts[allscripts.length - 1];
+  console.log('s',s);
+  s.parentNode.insertBefore(newscript, s);
+}
+
+
+function removeLogin() {
+    eraseCookie("tr_cookie");
+}
+
+function validateLogin() {
+    var logged_in = false;
+    var un = document.loginform.uname.value;
+    un = un.toLowerCase();
+    var pw = document.loginform.psw.value;
+    var guest_name = "guest";
+    var the_password_guest = "guest";
+    var the_un_enc = hash_of_string(un);
+    var the_url_enc = hash_of_string(window.location.hostname);
+    console.log("in validateLogin");
+    console.log(un);
+    console.log(pw);
+    console.log('window.location.hostname ' + window.location.hostname);
+    if ((typeof guest_access !== 'undefined') && guest_access && (un == guest_name) && (pw == the_password_guest)) {
+        console.log("setting the guest tr_cookie");
+        createCookie('tr_cookie',un,0.125);
+        logged_in = true;
+        console.log("logged in as guest", logged_in);
+    }
+    else if (pw == the_un_enc) {
+        console.log("setting the tr_cookie");
+        createCookie('tr_cookie',un,150);
+        logged_in = true;
+    }
+    else if (pw == the_url_enc) {
+        console.log("setting the tr_cookie");
+        createCookie('tr_cookie',un,1);
+        logged_in = true;
+    }
+    else {
+        alert ("Login was unsuccessful, please check id and password");
+        console.log("failed to set the tr_cookie");
+        logged_in = false;
+    }
+    console.log("logged_in", logged_in);
+    if (logged_in) {
+        document.getElementById('loginlogout').innerHTML = 'logout';
+        loadScript('answer');
+    }
+    return logged_in
+  }
+
+var this_id = readCookie('p_cookie');
+
+if (this_id) {
+    console.log("found cookie");
+    console.log(this_id);
+    var date = new Date();
+    var date_now = date.getTime();
+    var date_str = date_now.toString();
+    var date_str_trimmed = date_str.slice(5,13);
+    console.log(date_str)
+    console.log(date_str_trimmed)
+}
+else {
+    console.log("no cookie found")
+    var date = new Date();
+    var date_now = date.getTime();
+    var date_str = date_now.toString();
+    var date_str_trimmed = date_str.slice(5,13);
+    this_id = date_str_trimmed;
+    createCookie('p_cookie',this_id,150);
+}
+
+/* dataurlbase = dataurlbase.concat("per").concat("=").concat(this_id).concat("&");
+*/
+
+var a_id = readCookie('tr_cookie');
+
+window.addEventListener('load', function(){
+    console.log("checking login", a_id);
+if (a_id) {
+    console.log("found "+a_id);
+    $("#theloginform").hide();
+
+    console.log("repeating "+a_id);
+
+/*    dataurlbase = dataurlbase.concat("tr_id").concat("=").concat(a_id).concat("&");
+*/
+    document.getElementById('loginlogout').className = 'logout';
+    document.getElementById('loginlogout').innerHTML = 'logout';
+    console.log("done hiding "+a_id);
+    loadScript('answer');
+}
+else if (typeof login_required !== 'undefined' && login_required) {
+    console.log("show the generated login form");
+    login_form();
+}
+
+  $("#loginlogout.logout").click(function(){
+     console.log("make the logout form");
+     login_form('logout');
+    });
+  $("#loginlogout.login").click(function(){
+     console.log("make the login form");
+     login_form('login');
+    });
+});
