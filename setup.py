1e7100f002560a9618cc2db173edbbb09440a77c
Colin B. Macdonald <cbm@m.fsf.org>
2020-05-12 16:08:39 -0700

    Add setup.py and create mbx:main
    Add a basic setup.py: incomplete doesn't even try to install all the
    bits that might be needed.  But a place to start from.
    In mbx itself, a main() function is needed to have an "entry point"
    which will become a shell script during "pip install".

---
+++ setup.py
@@ -0,0 +1,58 @@
+import os
+from setuptools import setup, find_packages
+from glob import glob
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# This directory
+dir_setup = os.path.dirname(os.path.realpath(__file__))
+
+# TODO: is mbx versioned?  It should be
+#with open(os.path.join(dir_setup, "script", "version.py")) as f:
+#    # Defines __version__
+#    exec(f.read())
+__version__ = "0.0.1.dev"
+
+
+# Non-Python deps
+#   - TODO: Not sure about conventions...
+#   - TODO: but nice to list dependencies somewhere
+
+
+setup(
+    name="PreTeXt-utils",
+    version=__version__,
+    description="Utilities for processing PreTeXt files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pretextbook.org",
+    author="Rob Beezer",
+    license="GPLv2, GPLv3",  # TODO: check license
+    python_requires=">=3.6",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or GNU General Public License v3",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pretext-mbx=scripts.mbx:main",
+        ],
+    },
+    include_package_data=True,
+    data_files=[
+        (
+            "share/pretext/schema",
+            [
+                "schema/pretext.xml",
+            ],
+        ),
+    ],
+    install_requires = [
+        "Pillow",
+    ],
+)
