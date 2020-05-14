8a747a243be049959ed89f7eae47bb6cc2f4fa96
Colin B. Macdonald <cbm@m.fsf.org>
2020-05-14 13:21:45 -0700

    module needs a __init__ file

---
+++ script/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: FSFAP
+# Copyright (C) 2019-2020 Andrew Rechnitzer
+# Copyright (C) 2019-2020 Colin B. Macdonald
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+"""
+Utilities for PreTeXt files.
+"""
+
+__copyright__ = "Copyright (C) 2014-2020 Robert A. Beezer"
+__credits__ = "The PreTeXt Project Developers"
+__license__ = "GPL-2.0 or GPL-3.0"
