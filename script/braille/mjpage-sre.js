d172fb01c539db16ac3dc3cd303c92dcf3bf4c4c
Rob Beezer <beezer@ups.edu>
2020-06-08 20:55:45 -0700

    Improve MathJax/SRE script, produce Nemeth braille and speech

--- script/braille/mjpage-sre.js
+++ script/braille/mjpage-sre.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 // Original gist from Peter Krautzberger
 // https://gist.github.com/pkra/7ccdd351838f0bbdfe0080a4eacda7ca
 // Rob Beezer: 2019-02-14
@@ -8,6 +10,11 @@
 
 // Rob Beezer: 2019-07-24
 // Minor updates to accomodate SRE 3.0.0-beta.5
+
+// Rob Beezer: 2020-06-08
+// Add she-bang, so script is executable
+// Parameterize with argv[2]:  'nemeth', 'speech'
+// Place result in outerHTML, to match mathjax-node-page
 
 const fs = require('fs');
 const mjnode = require('mathjax-node-sre');
@@ -22,31 +29,37 @@ mjnode.config({
 });
 const mj = mjnode.typeset;
 
-const render = async (node) => {
+const render = async (node, format) => {
   let mathinput = node.outerHTML;
+  let params
+  if (format == 'nemeth') {
+    params = ['domain', 'default', 'locale', 'nemeth', 'modality', 'braille']
+  } else {
+    params = ['domain', 'default', 'locale', 'speech']
+  }
   const result = await mj({
     math: mathinput, // This is the MathML expression that will be converted
     format: "MathML",
     mml:true, // I think it does not matter which we pick, mml or svg; we are not using it anyway
-    sre: ['domain', 'default', 'locale', 'nemeth', 'modality', 'braille'], // This is important!
+    sre: params,
     });
 
   // result.speech contains Nemeth Braille code for mathinput
-  // This needs to be enclosed in some tags. I picked 'title';
-  // we may want to come up with something like <nemeth>.
-  node.innerHTML = '<nemeth>'+result.speech+'</nemeth>';
+  // *replace* the MathML "math" element
+  node.outerHTML = result.speech;
 };
 
 const main = async argv => {
-  const xhtml = fs.readFileSync(argv[2]).toString();
+  const xhtml = fs.readFileSync(argv[3]).toString();
   const dom = new JSDOM(xhtml, {
     contentType: 'application/xhtml+xml'
   });
   const document = dom.window.document;
   const nodes = document.querySelectorAll('math');
-  for (let node of nodes) await render(node)
+  const format = argv[2]
+  for (let node of nodes) await render(node, format)
   fs.writeFileSync(
-    argv[3],
+    argv[4],
     '<?xml version="1.0" encoding="utf-8"?>' + dom.serialize()
   );
 };
