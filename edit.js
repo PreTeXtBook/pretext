c9bdb079cebc11f5e1566cb910f4daaf30d86057
David Farmer <farmer@aimath.org>
2020-11-12 16:46:21 -0800

    pretty good progress

--- edit.js
+++ edit.js
@@ -126,21 +126,54 @@ $("p").click(function(){
 });
 */
 
+function container_for_editing(obj_type) {
+    var this_content_container = document.createElement('div');
+    this_content_container.setAttribute('id', "actively_editing");
+
+    if (obj_type == "paragraph") {
+        this_content_container.innerHTML = '<textarea id="actively_editing_paragraph" class="starting_point_for_editing" style="width:100%;" placeholder="' + obj_type + '"></textarea>';
+    } else if ( menu_for["theorem-like"].includes(obj_type) ) {
+        var title = "<div><b>" + obj_type + "&nbsp;NN</b>&nbsp;";
+        title += '<input id="actively_editing_title" class="starting_point_for_editing" placeholder="Optional title" type="text"/>';
+        title += '<input id="actively_editing_id" placeholder="Optional Id" class="input_id" type="text"/>';
+        title += '</div>';
+        var statement = '<textarea id="actively_editing_statement" style="width:100%;" placeholder="statement"></textarea>';
+        var proof = '<textarea id="actively_editing_proof" style="width:100%;" placeholder="proof (optional)"></textarea>';
+
+        this_content_container.innerHTML = title + statement + proof
+    }
+
+    return this_content_container
+}
+
+function hack_to_fix_first_textbox_character(thisID) {
+//    this_text = $("#" + thisID).val();
+//    console.log("this edit box" + this_text + "was");
+//    this_text = this_text.replace(/^\s/,"");
+//    this_text = this_text.replace(/^\s/,"");
+//    console.log("this edit box" + this_text + "is");
+//    $("#" + thisID).val(this_text);
+}
+
 function edit_in_place(obj) {
     thisID = obj.getAttribute("id");
+    console.log("will edit in place", thisID);
     if( sourceContent[thisID] ) {
+
+        var this_content_container = document.createElement('div');
+        this_content_container.setAttribute('id', "actively_editing");
+        this_content_container.setAttribute('data_source_id', thisID);
+        $("#" + thisID).replaceWith(this_content_container);
+
         var idOfEditContainer = thisID + '_input';
         var idOfEditText = thisID + '_input_text';
         var textarea_editable = document.createElement('textarea');
         textarea_editable.setAttribute('class', 'text_source');
         textarea_editable.setAttribute('id', idOfEditText);
-        textarea_editable.setAttribute('style', 'width:105%;');
+        textarea_editable.setAttribute('style', 'width:100%;');
 
-        var this_content_container = document.createElement('div');
-        this_content_container.setAttribute('id', idOfEditContainer);
-        $("#" + thisID).replaceWith(this_content_container);
-
-        document.getElementById(idOfEditContainer).insertAdjacentElement("afterbegin", textarea_editable);
+  //      document.getElementById(idOfEditContainer).insertAdjacentElement("afterbegin", textarea_editable);
+        document.getElementById('actively_editing').insertAdjacentElement("afterbegin", textarea_editable);
 
         $('#' + idOfEditText).val(sourceContent[thisID]);
         document.getElementById(idOfEditText).focus();
@@ -169,12 +202,9 @@ var sourceContent = {
     that is individually separate and distinct.'
 }
 
-console.log("adding tab listener");
+function menu_navigator(e) {  // we are not currently editing
+                              // so we are building the menu for the user to decide what/how to edit
 
-document.addEventListener('keydown', logKey);
-
-function logKey(e) {
-    console.log("logKey",e,"XXX",e.code);
     if (e.code == "Tab") {
        console.log("hit a Tab");
        console.log("focus is on", $(":focus"));
@@ -284,12 +314,13 @@ function logKey(e) {
             current_active_menu_item.setAttribute('class', 'chosen');
  //       current_active_menu_item.setAttribute('style', 'background:#ddf;');
             current_active_menu_item_environment = current_active_menu_item.getAttribute('data-env');
+
             if (current_active_menu_item_environment in menu_for) {  // object names a collection, so make submenu
                 console.log("making a menu for", current_active_menu_item_environment);
                 var edit_submenu = document.createElement('ol');
                 edit_submenu.innerHTML = base_menu_options_for(current_active_menu_item_environment);
-                console.log("removing id from", current_active_menu_item);
-                current_active_menu_item.removeAttribute("id");
+     //           console.log("removing id from", current_active_menu_item);
+     //           current_active_menu_item.removeAttribute("id");
                 current_active_menu_item.insertAdjacentElement("beforeend", edit_submenu);
           //      next_menu_item.setAttribute("id", "choose_current");
                 document.getElementById('choose_current').focus();
@@ -297,33 +328,40 @@ function logKey(e) {
                 next_menu_item.focus();
             } else {  // we just selected an action, so do it
                       // that probably involves adding something before or after a given object
+                e = "";
+                var new_object_type = current_active_menu_item.getAttribute("data-env");
                 object_near_new_object = document.getElementById('edit_menu_holder').parentElement;
                 var before_after = $("#edit_menu_holder > #edit_menu > .chosen").attr("data-location");
-                alert("attempting to add someting "+ before_after + " " + object_near_new_object.tagName);
-                object_near_new_object.focus();
+     //           alert("attempting to add " + new_object_type + " " + before_after + " " + object_near_new_object.tagName);
+                if (before_after == "before") { new_location = "beforebegin" }
+                else if (before_after == "after") { new_location = "afterend" }
+                object_near_new_object.insertAdjacentElement(new_location, container_for_editing(new_object_type));
+           //     document.getElementById('starting_point_for_editing').focus();
+                document.querySelectorAll('[class="starting_point_for_editing"]')[0].focus();
+                hack_to_fix_first_textbox_character('starting_point_for_editing');
+     //           object_near_new_object.focus();
                 object_near_new_object.classList.remove("may_select");
                 document.getElementById('edit_menu_holder').remove();
             }
 
-
-/*
-
-                if (['before', 'after'].includes(current_active_menu_item.getAttribute("data-location"))) {
-                    parent_type = document.getElementById('edit_menu_holder').parentElement.parentElement.tagName;
-                    console.log("making a menu for", parent_type);
-                    var edit_submenu = document.createElement('ol');
-                    edit_submenu.innerHTML = base_menu_options_for(parent_type);
-                    current_active_menu_item.insertAdjacentElement("beforeend", edit_submenu);
-                    next_menu_item = current_active_menu_item.nextSibling;
-
-                    console.log("next_menu_item",next_menu_item);
-                    // next 3 lines are repeats, so look to consolidate
-                    next_menu_item.setAttribute("id", "choose_current");
-                    console.log("setting focus AA on",next_menu_item);
-                    next_menu_item.focus();
-                }
-*/
         }
+    }
+}
+
+console.log("adding tab listener");
+
+document.addEventListener('keyup', logKey);
+
+function logKey(e) {
+    console.log("logKey",e,"XXX",e.code);
+    console.log("are we editing", document.getElementById('actively_editing'));
+    console.log("is there already an edit menu?", document.getElementById('edit_menu_holder'));
+
+    // if we are writing something, keystrokes usually are just text input
+    if (document.getElementById('actively_editing')) {
+
+    } else {
+        menu_navigator(e)
     }
 }
 
