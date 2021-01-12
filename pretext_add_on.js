f0085484fea3e4d27715685228f76f8e47c73eb0
David Farmer <farmer@aimath.org>
2021-01-11 20:25:45 -0800

    auto open knowls

--- pretext_add_on.js
+++ pretext_add_on.js
@@ -427,6 +427,26 @@ window.addEventListener("load",function(event) {
            }
 }});
 
+var knowl_id_counterX = 0;
+window.addEventListener("load",function(event) {
+   if($('body').hasClass("braillesample")) {
+       console.log("            found braillesample");
+       var all_knowls = $('[data-knowl]');
+       console.log("found", all_knowls.length, "knowls");
+       console.log("which are", all_knowls);
+       for (var j=1; j < all_knowls.length; ++j) {
+           console.log(j, "un-knowling", all_knowls[j]);
+           console.log("attr", $(all_knowls[j]).attr("data-knowl"));
+           $knowl = $(all_knowls[j]);
+           if(!$knowl.attr("data-knowl-uid")) {
+              $knowl.attr("data-knowl-uid", knowl_id_counterX);
+              knowl_id_counterX++;
+            }
+            knowl_click_handler($knowl);
+          // knowl_click_handler($(all_knowls[j]))
+       }
+}});
+
 /*
 window.setInterval(function(){
     console.log('$(":focus")', $(":focus"));
