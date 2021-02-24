757d2fafba46435db989292fd661932b05378b9a
Rob Beezer <beezer@ups.edu>
2021-02-24 11:16:53 -0800

    Script: improve error-reporting from XSLT application

--- pretext/pretext.py
+++ pretext/pretext.py
@@ -1544,13 +1544,21 @@ def xsltproc(xsl, xml, result, output_dir=None, stringparams={}):
         os.chdir(output_dir)
     # clear global errors, apply the xsl transform
     ET.clear_error_log()
-    result_tree = xslt(src_tree, **stringparams)
-    # report any errors
-    messages = xslt.error_log
-    if messages:
-        print('Messages from application of {}:'.format(xsl))
-        for m in messages:
-            print(m.message)
+    try:
+        result_tree = xslt(src_tree, **stringparams)
+        # report any messages, even if successful (indented)
+        messages = xslt.error_log
+        if messages:
+            print('PTX: Successful application of {}, but with messages:'.format(xsl))
+            for m in messages:
+                print('    * ', m.message)
+    except:
+        # report any errors on failure (indented)
+        messages = xslt.error_log
+        if messages:
+            print('PTX: Failed application of {}, with messages:'.format(xsl))
+            for m in messages:
+                print('    * ', m.message)
     os.chdir(owd)
 
     # write a serialized version to a file if
