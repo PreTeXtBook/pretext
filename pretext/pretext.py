46ee008d3afaa444334176e45f40b124355b75f9
Rob Beezer <beezer@ups.edu>
2022-11-18 18:00:51 -0800

    Script: stop managing publisher file specification inside WeBWorK function

--- pretext/pretext.py
+++ pretext/pretext.py
@@ -910,12 +910,9 @@ def webwork_to_xml(
     log.info(
         "string parameters passed to extraction stylesheet: {}".format(stringparams)
     )
-    effective_pub_file = (
-        stringparams["publisher"] if "publisher" in stringparams else pub_file
-    )
 
     # Either we have a "generated" directory, or we must assume placing everything in dest_dir
-    generated_dir, _ = get_managed_directories(xml_source, effective_pub_file)
+    generated_dir, _ = get_managed_directories(xml_source, pub_file)
     if generated_dir:
         ww_reps_dir = os.path.join(generated_dir, "webwork")
         ww_images_dir = os.path.join(ww_reps_dir, "images")
