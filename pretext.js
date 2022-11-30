356e4f9542674d2e3cba158c100047cdd3dc51d4
David Farmer <farmer@aimath.org>
2022-11-29 19:32:10 -0800

    make it possible to unhide the TOC

--- pretext.js
+++ pretext.js
@@ -20,5 +20,27 @@ function toggletoc() {
 window.addEventListener("load",function(event) {
        thetocbutton = document.getElementsByClassName("toc-toggle")[0];
        thetocbutton.addEventListener('click', () => toggletoc() );
+       thepage = document.getElementsByClassName("ptx-page")[0];
+       console.log("thepage", thepage);
+       console.log("width", thepage.offsetWidth);
+       if (thepage.offsetWidth < 800) {
+           toggletoc()
+       }
 });
 
+window.addEventListener("load",function(event) {
+       pagefilename  = window.location.href;
+       pagefilename  = pagefilename.match(/[^\/]*$/)[0];
+       possibletocentries = document.querySelectorAll('#ptx-toc a[href="' + pagefilename + '"]');
+       if (possibletocentries.length == 0) {
+           console.log("linked below a subsection");
+           pagefilename  = pagefilename.match(/^[^\#]*/)[0];
+           possibletocentries = document.querySelectorAll('#ptx-toc a[href="' + pagefilename + '"]');
+       }
+       if (possibletocentries.length == 0) {
+           console.log("error, cannot find", pagefilename, "in TOC");
+           return
+       }
+       possibletocentries[0].scrollIntoView({block: "center"});
+       possibletocentries[0].classList.add("active");
+});
