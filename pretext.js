c1fa3ee045f017d9e85083487a0fc9bc73031590
David Farmer <farmer@aimath.org>
2022-12-19 18:50:30 -0800

    some TOC things

--- pretext.js
+++ pretext.js
@@ -13,21 +13,32 @@
 
 function toggletoc() {
    thesidebar = document.getElementById("ptx-sidebar");
-   thesidebar.classList.toggle("hidden");
+   if (thesidebar.classList.contains("hidden") || thesidebar.classList.contains("visible")) {
+       thesidebar.classList.toggle("hidden");
+       thesidebar.classList.toggle("visible");
+   } else if (thesidebar.offsetParent === null) {  /* not currently visible */
+       thesidebar.classList.toggle("visible");
+   } else {
+       thesidebar.classList.toggle("hidden");
+   }
+/*
    themain = document.getElementsByClassName("ptx-main")[0];
    themain.classList.toggle("notoc");
    console.log("toggled the toc");
+*/
 }
 
 window.addEventListener("load",function(event) {
        thetocbutton = document.getElementsByClassName("toc-toggle")[0];
        thetocbutton.addEventListener('click', () => toggletoc() );
+/*
        thepage = document.getElementsByClassName("ptx-page")[0];
        console.log("thepage", thepage);
        console.log("width", thepage.offsetWidth);
        if (thepage.offsetWidth < 800) {
            toggletoc()
        }
+*/
 });
 
 window.addEventListener("load",function(event) {
@@ -46,3 +57,23 @@ window.addEventListener("load",function(event) {
        possibletocentries[0].scrollIntoView({block: "center"});
        possibletocentries[0].classList.add("active");
 });
+
+/* jump to next page if reader tries to scroll past the bottom */
+// var hitbottom = false;
+// window.onscroll = function(ev) {
+//   if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+//     // you're at the bottom of the page
+//     console.log("Bottom of page");
+//     if (hitbottom) {
+//         console.log("hit bottom again");
+//         thenextbutton = document.getElementsByClassName("next-button")[0];
+//         thenextbutton.click();
+//     } else {
+//         hitbottom = true;
+//         /* only jump to next page if hard scroll in quick succession */
+//         window.scrollBy(0, -20);
+//         setTimeout(function (){ hitbottom = false }, 1000);
+//     }
+//   }
+// };
+
