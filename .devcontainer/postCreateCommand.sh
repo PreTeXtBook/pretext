ff10237a9bb808bf269a5ec75ec891c65c306747
Steven Clontz <steven.clontz@gmail.com>
2023-03-24 18:59:23 +0000

    support full build codespace

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt update
+
+echo "Install LaTeX"
+apt install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
+
+echo "Install sage"
+apt install -y sagemath --no-install-recommends
+
+echo "Install PDF tools"
+apt install -y ghostscript pdf2svg --no-install-recommends
