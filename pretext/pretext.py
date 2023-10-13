f034a713251c26fbb70a35aa58af14e572240dd9
Steven Clontz <steven.clontz@gmail.com>
2023-10-13 12:07:17 -0500

    remove kwarg for manage directories in latex

--- pretext/pretext.py
+++ pretext/pretext.py
@@ -3424,7 +3424,7 @@ def assembly(xml, pub_file, stringparams, out_file, dest_dir, method):
 #####################
 
 
-def latex(xml, pub_file, stringparams, extra_xsl, out_file, dest_dir, use_manage_directories=False):
+def latex(xml, pub_file, stringparams, extra_xsl, out_file, dest_dir):
     """Convert XML source to LaTeX in destination directory"""
 
     # to ensure provided stringparams aren't mutated unintentionally
@@ -3444,11 +3444,6 @@ def latex(xml, pub_file, stringparams, extra_xsl, out_file, dest_dir, use_manage
     # Write output into working directory, no scratch space needed
     log.info("converting {} to LaTeX as {}".format(xml, derivedname))
     xsltproc(extraction_xslt, xml, derivedname, None, stringparams)
-
-    if use_manage_directories:
-        # Manage directories so LaTeX can be built in-place
-        external_abs, generated_abs = get_managed_directories(xml, pub_file)
-        manage_directories(dest_dir, external_abs=external_abs, generated_abs=generated_abs)
 
 
 ###################
