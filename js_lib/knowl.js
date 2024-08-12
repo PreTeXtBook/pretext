34d8e4f32e44f977c8c8cae32b5007b61e97eb60
Cox Geoffrey W <63681414+geoff-cox@users.noreply.github.com>
2024-08-12 09:36:44 -0400

    HTML: improve rendering knowls having MathJax content (PR #2247)

--- js_lib/knowl.js
+++ js_lib/knowl.js
@@ -56,8 +56,11 @@ class SlideRevealer {
       this.animatedElement.setAttribute("open","");
       this.triggerElement.setAttribute("open","");
       this.contentElement.style.display = '';
-      // Wait for the next frame to call the toggle function
-      window.requestAnimationFrame(() => this.toggle(true));
+      // Trigger the animation to expand or collapse the knowl.
+      // Delay the MathJax typesetting until the knowl is visible to ensure proper measurements
+      // are taken, but before the unrolling begins. This helps avoid layout shifts and ensures
+      // smooth animation with correctly sized content.
+      MathJax.typesetPromise().then(() => window.requestAnimationFrame(() => this.toggle(true)));
     } else if (this.animationState === SlideRevealer.STATE.EXPANDING || this.animatedElement.hasAttribute("open")) {
       this.toggle(false);
     }
@@ -312,7 +315,6 @@ class LinkKnowl {
           this.outputElement.append(...children);
 
           // render any knowls and mathjax in the knowl
-          MathJax.typesetPromise([this.outputElement]);
           addKnowls(this.outputElement);
 
           // try prism highlighting
