1de95dbbdb99e8af93b0bc1b5d475b934a1aaa19
Andrew Scholer <ascholer@chemeketa.edu>
2024-11-04 09:16:48 -0800

    Runestone: update generated trace files to use new ids (PR #2288)

--- examples/sample-book/gen/trace/python-hello-world-code-lens.js
+++ examples/sample-book/gen/trace/python-hello-world-code-lens.js
@@ -1,4 +1,4 @@
 if (allTraceData === undefined) {
  var allTraceData = {};
  }
- allTraceData["python-hello-world-code-lens"] = {"code": "print('Hello, World!')\n\n", "trace": [{"line": 1, "event": "step_line", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": ""}, {"line": 1, "event": "return", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": "Hello, World!\n"}]}
+ allTraceData["rs-python-hello-world-code-lens"] = {"code": "print('Hello, World!')\n\n", "trace": [{"line": 1, "event": "step_line", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": ""}, {"line": 1, "event": "return", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": "Hello, World!\n"}]}
