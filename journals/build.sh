d17a7ae533b85be9d85c1ec1f9d14514bdec9705
Oscar Levin <oscar.levin@unco.edu>
2025-04-03 22:18:55 -0600

    Journals: add texstyle files and update list of journals

--- journals/build.sh
+++ journals/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# When journals.xml is updated, run this script to create a pretext table in the guide's appendices folder which "journals.xml" will import.
+# When journals.xml is updated, run this script from the journals directory to create a pretext table in the guide's appendices folder which "journals.xml" will import.
 
 # Build the journals-table for the documentation
 xsltproc journals-to-table.xsl journals.xml > ../doc/guide/appendices/journals-table.xml
