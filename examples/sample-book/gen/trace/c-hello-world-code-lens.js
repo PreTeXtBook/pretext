16715fb3b944196f7d6639400f5aee73332ac92c
Andrew Scholer <ascholer@chemeketa.edu>
2025-07-17 14:48:00 -0700

    Sample book: update codelens samples, add new checkpoint samples

--- examples/sample-book/gen/trace/c-hello-world-code-lens.js
+++ examples/sample-book/gen/trace/c-hello-world-code-lens.js
@@ -1,5 +1,12 @@
-if (allTraceData === undefined) {
- var allTraceData = {};
- }
- allTraceData["rs-c-hello-world-code-lens"] = {"code": "#include <stdio.h>\n\nint main(void)\n{\n    puts(\"Hello, World!\");\n}\n\n", "trace": [{"event": "step_line", "func_name": "main", "globals": {}, "heap": {}, "line": 5, "ordered_globals": [], "stack_to_render": [{"encoded_locals": {}, "frame_id": "0xFFF000BE0", "func_name": "main", "is_highlighted": true, "is_parent": false, "is_zombie": false, "line": 4, "ordered_varnames": [], "parent_frame_id_list": [], "unique_hash": "main_0xFFF000BE0"}], "stdout": ""}, {"event": "return", "func_name": "main", "globals": {}, "heap": {}, "line": 6, "ordered_globals": [], "stack_to_render": [{"encoded_locals": {}, "frame_id": "0xFFF000BE0", "func_name": "main", "is_highlighted": true, "is_parent": false, "is_zombie": false, "line": 6, "ordered_varnames": [], "parent_frame_id_list": [], "unique_hash": "main_0xFFF000BE0"}], "stdout": "Hello, World!\n"}]}
 
+if (allTraceData === undefined) {
+    var allTraceData = {};
+}
+(function() { // IIFE to avoid variable collision
+    let codelensID = "rs-c-hello-world-code-lens";  //fallback
+    let partnerCodelens = document.currentScript.parentElement.querySelector(".pytutorVisualizer");
+    if (partnerCodelens) {
+        codelensID = partnerCodelens.id;
+    }
+    allTraceData[codelensID] = {"code": "#include <stdio.h>\n\nint main(void)\n{\n    puts(\"Hello, World!\");\n}\n", "trace": [{"event": "step_line", "func_name": "main", "globals": {}, "heap": {}, "line": 5, "ordered_globals": [], "stack_to_render": [{"encoded_locals": {}, "frame_id": "0xFFF000BE0", "func_name": "main", "is_highlighted": true, "is_parent": false, "is_zombie": false, "line": 4, "ordered_varnames": [], "parent_frame_id_list": [], "unique_hash": "main_0xFFF000BE0"}], "stdout": ""}, {"event": "return", "func_name": "main", "globals": {}, "heap": {}, "line": 6, "ordered_globals": [], "stack_to_render": [{"encoded_locals": {}, "frame_id": "0xFFF000BE0", "func_name": "main", "is_highlighted": true, "is_parent": false, "is_zombie": false, "line": 6, "ordered_varnames": [], "parent_frame_id_list": [], "unique_hash": "main_0xFFF000BE0"}], "stdout": "Hello, World!\n"}], "startingInstruction": 0};
+})();
