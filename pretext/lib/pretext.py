3894e59fe550a2043961605865f14004abac9965
Rob Beezer <beezer@ups.edu>
2025-08-16 14:50:40 -0700

    Script: initiate procedure for converting STACK questions

--- pretext/lib/pretext.py
+++ pretext/lib/pretext.py
@@ -2719,6 +2719,58 @@ def mermaid_images(
                     ]
                     log.warning("\n".join(msg))
 
+
+#####################################
+#
+#  STACK exercises (static)
+#
+#####################################
+
+# 2025-08-16: verbatim from
+#   https://github.com/PreTeXtBook/pretext/pull/2576
+# procedure name modified
+def _stack_replace_latex(text):
+    text = re.sub(r"\\\((.*?[^\\])\\\)", r"<m>\1</m>", text)
+    text = re.sub(r"\\\[(.*?[^\\])\\]", r"<me>\1</me>", text)
+    # We may want to detect align/similar environments inside \[\] and replace them with
+    # <md></md> using <mrow></mrow> for each row and \amp for alignment (also \lt, \gt)
+    return text
+
+# 2025-08-16: verbatim from
+#   https://github.com/PreTeXtBook/pretext/pull/2576
+# procedure name modified
+def _stack_process_response(qdict):
+    # This is a new feature not yet available at https://stack-api.maths.ed.ac.uk/render
+    # if qdict["isinteractive"]:
+    #     # We could generate a QR code to an online version in the future
+    #     return "<statement><p>This question contains interactive elements.</p></statement>"
+    qtext = qdict["questionrender"]
+    soltext = qdict["questionsamplesolutiontext"]
+
+    # Strip validation and specific feedback
+    qtext = re.sub("\[\[validation:(\w+)\]\]", "", qtext)
+    qtext = re.sub("\[\[feedback:(\w+)\]\]", "", qtext)
+
+    # Iterate over inputs. For each input with ID ansid:
+    ansids = re.findall("\[\[input:(\w+)\]\]", qtext)
+    answers = []
+    for ansid in ansids:
+        ansdata = qdict["questioninputs"][ansid]
+
+        ansconfig = ansdata["configuration"]
+        width = ansconfig["boxWidth"]
+
+        answers.append(f'<p><m>{ansdata["samplesolutionrender"]}</m></p>') # still need to wrap into <answer></answer>
+        qtext = qtext.replace(f"[[input:{ansid}]]", f'<fillin characters="{width}" name="{ansid}">')
+
+    qtext = _stack_replace_latex(qtext)
+    soltext = _stack_replace_latex(soltext)
+
+    return f'''
+    <statement>{qtext}</statement>
+    <solution>{soltext}</solution>
+    ''' + "\n".join(f"<answer>{ans}</answer>" for ans in answers)
+
 #####################################
 #
 #  Interactive preview screenshotting
--- pretext/lib/stack_to_pretext.py
+++ pretext/lib/stack_to_pretext.py
@@ -3,47 +3,6 @@ import json
 import re
 
 
-def replace_latex(text):
-    text = re.sub(r"\\\((.*?[^\\])\\\)", r"<m>\1</m>", text)
-    text = re.sub(r"\\\[(.*?[^\\])\\]", r"<me>\1</me>", text)
-    # We may want to detect align/similar environments inside \[\] and replace them with
-    # <md></md> using <mrow></mrow> for each row and \amp for alignment (also \lt, \gt)
-    return text
-
-
-def process_response(qdict):
-    # This is a new feature not yet available at https://stack-api.maths.ed.ac.uk/render
-    # if qdict["isinteractive"]:
-    #     # We could generate a QR code to an online version in the future
-    #     return "<statement><p>This question contains interactive elements.</p></statement>"
-    qtext = qdict["questionrender"]
-    soltext = qdict["questionsamplesolutiontext"]
-
-    # Strip validation and specific feedback
-    qtext = re.sub("\[\[validation:(\w+)\]\]", "", qtext)
-    qtext = re.sub("\[\[feedback:(\w+)\]\]", "", qtext)
-
-    # Iterate over inputs. For each input with ID ansid:
-    ansids = re.findall("\[\[input:(\w+)\]\]", qtext)
-    answers = []
-    for ansid in ansids:
-        ansdata = qdict["questioninputs"][ansid]
-
-        ansconfig = ansdata["configuration"]
-        width = ansconfig["boxWidth"]
-
-        answers.append(f'<p><m>{ansdata["samplesolutionrender"]}</m></p>') # still need to wrap into <answer></answer>
-        qtext = qtext.replace(f"[[input:{ansid}]]", f'<fillin characters="{width}" name="{ansid}">')
-
-    qtext = replace_latex(qtext)
-    soltext = replace_latex(soltext)
-
-    return f'''
-    <statement>{qtext}</statement>
-    <solution>{soltext}</solution>
-    ''' + "\n".join(f"<answer>{ans}</answer>" for ans in answers)
-
-
 def process_stack_XML(filename, api_url, seed=None):
     qdata = open(filename).read()
     req_data = {
