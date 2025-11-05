6e685595d5967d5d25eae299aab96dde54d89445
Oscar Levin <oscar.levin@unco.edu>
2025-11-04 22:31:06 -0700

    Javascript: fix incorrect pagebreaks in worksheets with tasks (PR #2716)

--- js/pretext_add_on.js
+++ js/pretext_add_on.js
@@ -674,8 +674,9 @@ function createPrintoutPages(margins) {
             // Keep the child as a block, but put each task after the first one as its own row:
             rows.push(child);
             const tasks = child.querySelectorAll('.task');
-            for (let i = 1; i < tasks.length; i++) {
-                rows.push(tasks[i]);
+            for (let i = tasks.length-1; i > 0; i--) {
+                // Move the task out of the original child and place it directly after it in the printout.  We do this in reverse order so when every task is moved, they return to the original order. They will then be added to the rows list as their own blocks.
+                printout.insertBefore(tasks[i], child.nextSibling);
             }
         // Skipping separate treatment of exercisegroups for now.
         //} else if (child.classList.contains('exercisegroup')) {
