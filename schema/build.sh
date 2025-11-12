7523a9f3c8455420702a14f47ad24408be6fc7ba
Rob Beezer <beezer@ups.edu>
2025-11-12 08:47:19 -0800

    Schema: stop generating schema browser with FlexDoc

--- schema/build.sh
+++ schema/build.sh
@@ -27,6 +27,7 @@
 #  2017-07-21  For release
 #  2019-05-24  Updates as DocFlex becomes FlexDoc (and v1.12.3)
 #  2020-11-11  FlexDoc v1.12.5
+#  2025-11-12  Removed FlexDoc in favor of Siefken's browser
 
 #  This is designed for use in distributing derived products
 #  from the PreTeXt schema.  So make a copy and adjust paths
@@ -40,14 +41,6 @@ shopt -s -o nounset
 
 # PreTeXt distribution
 declare MB=${HOME}/mathbook/mathbook
-# FlexDoc installation
-declare FDH=/opt/flexdoc/flexdoc-xml-1.12.5
-
-# DocFlex output directory
-declare FDOUTDIR=${HOME}/mathbook/website/pretextbook.org/doc/schema
-# Java root to locate executables
-# (if not set by system: uncomment and set)
-declare JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 # *************
 # Derived paths
@@ -55,10 +48,6 @@ declare JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 # XSL for literate programming tool
 declare MBXSL=${MB}/xsl
-# Java particulars for documentation generation
-# Quotes to protect spaces (use on filenames?)
-declare JAVA_OPTIONS="-Xms512m -Xmx1024m"
-declare CLASS_PATH="${FDH}/lib/xml-apis.jar:${FDH}/lib/xercesImpl.jar:${FDH}/lib/resolver.jar:${FDH}/lib/flexdoc-xml.jar:/usr/share/openjfx/lib/*"
 
 # ******************
 # Grammar generation
@@ -79,24 +68,6 @@ trang -o disable-abstract-elements -I rnc -O xsd pretext.rnc pretext.xsd
 xsltproc ${MBXSL}/pretext-litprog.xsl publication-schema.xml
 trang -I rnc -O rng publication-schema.rnc publication-schema.rng
 trang -o disable-abstract-elements -I rnc -O xsd publication-schema.rnc publication-schema.xsd
-
-# ************************
-# Documentation Generation
-# ************************
-#
-# We use DocFlex/XML - XSDDoc - XML Schema Documentation Generator
-#
-# http://www.filigris.com/docflex-xml/xsddoc/
-#
-# Execution and options cribbed from DocFlex distribution
-#
-# -docflexconfig specifies a DocFlex Linux-specific configuration
-#  v1.12 config now in bin directory
-#  v1.12 requires Oracle Java (ie OpenJDK lacks "javafx")
-
-${JAVA_HOME}/bin/java ${JAVA_OPTIONS} -cp ${CLASS_PATH} xyz.flexdoc.xml.Generator \
-    -flexdocconfig ${FDH}/bin/linux/flexdoc.config -quiet \
-    -nodialog -launchviewer=false -d ${FDOUTDIR} pretext.xsd
 
 # exit cleanly
 exit 0
