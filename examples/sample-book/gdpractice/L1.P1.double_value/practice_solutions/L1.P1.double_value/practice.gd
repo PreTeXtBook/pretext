06a8403f6c64443d0a37ce5a9c0c6c626b4e8e8c
DavidCooperWCU <dcooper@wcupa.edu>
2026-08-03 11:28:33 -0400

    Sample book: add gdscript samples

---
+++ examples/sample-book/gdpractice/L1.P1.double_value/practice_solutions/L1.P1.double_value/practice.gd
@@ -0,0 +1,9 @@
+# L1.P1 — Double the value
+#
+# Complete get_double() so that it returns twice its input.
+extends Node
+
+
+# Returns double `value`.
+func get_double(value: int) -> int:
+	return value * 2 # return value
